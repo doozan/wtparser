6bf86f37c9528feb3f338e42a1612db4d36b2214
Jeff Doozan <github@doozan.com>
2020-10-30 11:16:26 -0500

    Add support for reflexive inflections and inflect_combined

--- languages/es/__init__.py
+++ languages/es/__init__.py
@@ -22,6 +22,7 @@ import re
 import sys
 
 from .paradigms import paradigms
+from .combined import _data as combined
 from .. import LanguageData
 
 class Data(LanguageData):
@@ -455,42 +456,160 @@ class Data(LanguageData):
             return {}
         ending = "-" + str(template.get(2)).strip()
 
+        # Find the nearest ancestor with a Conjugation section
         matcher = lambda x: callable(getattr(x, "filter_sections", None)) and \
-                any(x.filter_sections(matches=lambda y: y.name.strip() == "Conjugation"))
-        ancestor = word if matcher(word) else word.get_matching_ancestor(matcher)
+                any(x.filter_sections(matches=lambda y: y.name.strip().startswith("Conjugation")))
+        ancestor = word.get_matching_ancestor(matcher)
         if not ancestor:
-            print("No conjugations", word.page_title, file=sys.stderr)
+            if " " not in word.page_title and not word.page_title.endswith("se"):
+                print("\nNo conjugations", word.page_title, file=sys.stderr)
             return {}
 
         all_forms = {}
-        for conjugation in ancestor.ifilter_sections(matches=lambda x: x.name.strip() == "Conjugation"):
+        for conjugation in ancestor.ifilter_sections(matches=lambda x: x.name.strip().startswith("Conjugation")):
             for t in conjugation.ifilter_templates(matches=lambda x: x.name.strip().startswith("es-conj")):
                 pattern = None
+                reflexive = t.has("ref")
                 if t.has("p"):
                     pattern = str(t.get("p").value)
                 stems = [ str(p.value).strip() for p in t.params if str(p.name).isdigit() ]
 
-                for formtype, forms in cls.do_conjugate(stems, ending, pattern).items():
+                inflections = cls.inflect(stems, ending, pattern, reflexive)
+                combined = cls.inflect_combined(inflections, ending, pattern, reflexive)
+
+                for formtype, forms in combined.items():
+                    for form in forms:
+                        if formtype not in inflections:
+                            inflections[formtype] = set()
+                        inflections[formtype].add(form)
+
+                        if "_acc_" not in formtype:
+                            continue
+
+                        formtype = formtype.replace("_acc_", "_acc-dat_")
+
+                        if formtype not in inflections:
+                            inflections[formtype] = set()
+
+                        for indirect_pronoun in [ "lo", "la", "los", "las" ]:
+                            if form[-2:] == "le":
+                                form_modified = form[:-2] + "se" + indirect_pronoun
+                            elif form[-3:] == "les":
+                                form_modified = form[:-3] + "se" + indirect_pronoun
+                            else:
+                                form_modified = form + indirect_pronoun
+
+                            # if original form is unstressed (eg, it only had two vowels: dame), add stress
+                            if not re.match("[áéíóú]", form_modified):
+                                form_modified = cls.create_accented_form(form_modified)
+                            inflections[formtype].add(form_modified)
+
+
+
+                for formtype, forms in inflections.items():
                     formtype = str(formtype)
+#                    formtype = "conj"
                     for form in forms:
                         if not form:
                             continue
                         if formtype not in all_forms:
-                            all_forms[formtype] = [form]
-                        elif form not in all_forms[formtype]:
-                            all_forms[formtype].append(form)
+                            all_forms[formtype] = set()
+                        all_forms[formtype].add(form)
 
         return all_forms
 
+
+    vowels = {
+        "a": "á",
+        "e": "é",
+        "i": "í",
+        "o": "ó",
+        "u": "ú"
+        }
+    stressed_vowels = {
+        "á": "a",
+        "é": "e",
+        "í": "i",
+        "ó": "o",
+        "ú": "u"
+        }
+
     @classmethod
-    def do_conjugate(self, stems, ending, pattern):
+    def create_accented_form(cls, form, ua_disyllabic=False):
+        """ create accented stem for reflexive verbs """
+        if not form:
+            return form
+
+        if "," in form:
+            res = []
+            for subform in form.split(","):
+                res.append(cls.create_accented_form(subform, ua_disyllabic))
+            return ",".join(res)
+
+        result = form
+
+        vowel_count = 0
+        for c in range(len(result)-1,-1,-1):
+            v1 = result[c]
+            if v1 not in cls.vowels:
+                continue
+
+            vowel_count +=1
+
+            # 'ue' or 'ua' count as 1 vowel, accent goes on e or a
+            if c<len(result)-1:
+                v2 = result[c+1]
+
+                if v1 == 'u' and v2 == 'e':
+                    vowel_count -= 1
+
+                if v1 == 'u' and v2 == 'a' and not ua_disyllabic:
+                    vowel_count -= 1
+
+            # 'ai' or 'oi' counts as 1 vowel, accent goes on a or o
+            if c>1:
+                v3 = result[c-1]
+                if v3 == 'a' and v1 == 'i':
+                    vowel_count -= 1
+                if v3 == 'o' and v1 == 'i':
+                    vowel_count -= 1
+
+            if vowel_count == 3:
+                return result[:c] + cls.vowels[v1] + result[c+1:]
+
+        return result
+
+
+
+    @classmethod
+    def inflect(cls, stems, ending, pattern="", reflexive=False):
+
+        if pattern is None:
+            pattern = ""
+
+        if ending not in paradigms:
+            print("Bad verb ending", stems,ending,pattern, file=sys.stderr)
+            return {}
+
+        if pattern and pattern not in paradigms[ending]:
+            print("Bad verb pattern", stems,ending,pattern, file=sys.stderr)
+            return {}
+
+        pattern_data = paradigms[ending][pattern]
 
         # This has to be a deep copy, since we're overwriting values
         data = {k:v for k, v in paradigms[ending]['']['patterns'].items()}
 
+        if reflexive:
+            for k,v in paradigms[ending]['']['ref'].items():
+                data[k] = v
+
+            if pattern and 'ref' in pattern_data:
+                for k,v in pattern_data['ref'].items():
+                    data[k] = v
+
         # Layer pattern data over base data
         if pattern:
-            pattern_data = paradigms[ending][pattern]
 
             if 'replacement' in pattern_data:
                 for pk,pv in pattern_data['replacement'].items():
@@ -504,6 +623,11 @@ class Data(LanguageData):
                 else:
                     data[pk] = pv
 
+        if reflexive:
+            for k in [63,65,68]:
+                if not pattern_data.get("unstressed",{}).get(k):
+                    data[k] = cls.create_accented_form(data[k])
+
         for dk,dv in data.items():
             if dv:
                 if len(stems):
@@ -514,4 +638,92 @@ class Data(LanguageData):
                 data[dk] = [ None ]
 
         return data
+
+
+    @classmethod
+    def inflect_combined(cls, forms, ending, pattern="", reflexive=False):
+
+        if pattern is None:
+            pattern = ""
+
+        if ending not in paradigms:
+            print("Bad verb ending", ending, pattern, file=sys.stderr)
+            return {}
+
+        if pattern and pattern not in paradigms[ending]:
+            print("Bad verb pattern", ending, pattern, file=sys.stderr)
+            return {}
+
+        pattern_data = paradigms[ending][pattern]
+        unstressed = pattern_data.get("unstressed", {})
+
+        # This has to be a deep copy, since we're overwriting values
+        data = {k:v for k, v in paradigms[ending]['']['patterns'].items()}
+
+        result = {}
+        aspects = ["acc", "dat"]
+
+        for paradigm, paradigm_data in combined.items():
+            for form in forms.get(paradigm_data["index"]):
+                if reflexive:
+                    form = form[:-2]
+                for aspect in aspects:
+                    for k2, pronoun_table in enumerate(paradigm_data[aspect],1):
+
+                        t = []
+                        for k3, pronoun in enumerate(pronoun_table,1):
+                            irregular = False
+                            form_modified = form
+
+                            stem_cuts = paradigm_data.get("stem_cuts",{}).get(k2)
+                            if stem_cuts:
+                                if len(stem_cuts) == 2:
+                                    form_modified = form_modified[stem_cuts[0]-1:stem_cuts[1]+1]
+                                else:
+                                    if pronoun == stem_cuts[0]:
+                                        form_modified = form_modified[stem_cuts[1]-1:stem_cuts[2]+1]
+
+                            ending_irregularities = paradigm_data.get("ending_irregularities",{}).get(ending)
+                            if ending_irregularities:
+                                for pattern_data in ending_irregularities:
+                                    if aspect == pattern_data[0]:
+                                        if k2 == pattern_data[1]:
+                                            if pronoun == pattern_data[2]:
+                                                form_modified = pattern_data[3](form_modified)
+
+                            paradigm_irregularities = paradigm_data.get("paradigm_irregularities",{}).get(pattern)
+                            if paradigm_irregularities:
+                                for pattern_data in paradigm_irregularities:
+                                    if aspect == pattern_data[0]:
+                                        if k2 == pattern_data[1]:
+                                            if pronoun == pattern_data[2]:
+                                                form_modified = pattern_data[3](form_modified)
+                                                irregular = True
+
+                            if unstressed:
+                                if unstressed.get(paradigm_data["index"], False):
+                                    form_modified = cls.unstress(form_modified)
+
+                            if paradigm_data.get("paradigm_no_accent", {}).get(pattern):
+                                form_modified = cls.unstress(form_modified) + pronoun
+                            else:
+                                if not irregular:
+                                    if paradigm_data.get("accented_stem"):
+                                        if paradigm_data.get("ua_disyllabic",{}).get(pattern):
+                                            form_modified = cls.create_accented_form(cls.unstress(form_modified+pronoun), True)
+                                        else:
+                                            form_modified = cls.create_accented_form(cls.unstress(form_modified+pronoun), False)
+                                    else:
+                                        form_modified = form_modified + pronoun
+                            t += form_modified.split(", ")
+
+                        if t:
+                            parameter = paradigm + "_" + aspect + "_" + str(k2)
+                            if parameter in result:
+                                result[parameter] += t
+                            else:
+                                result[parameter] = t
+
+        return result
+
 
--- languages/es/tests/test_es.py
+++ languages/es/tests/test_es.py
@@ -2,7 +2,6 @@ from .... import parse_page
 from ....utils import parse_anything
 from .. import Data
 
-
 def test_match_headword():
     template = parse_anything("{{es-noun|m}}").filter_templates()[0]
     assert Data.match_headword(template) == True
@@ -133,7 +132,10 @@ def test_get_verb_forms1():
     wikt = parse_page(data, "tener", None)
     word = next(wikt.ifilter_words())
     forms = Data.get_forms(word)
-    assert forms == {'1': ['tener'], '2': ['teniendo'], '3': ['tenido'], '4': ['tenida'], '5': ['tenidos'], '6': ['tenidas'], '7': ['tengo'], '8': ['tienes'], '9': ['tenés'], '10': ['tiene'], '11': ['tenemos'], '12': ['tenéis'], '13': ['tienen'], '14': ['tenía'], '15': ['tenías'], '16': ['tenía'], '17': ['teníamos'], '18': ['teníais'], '19': ['tenían'], '20': ['tuve'], '21': ['tuviste'], '22': ['tuvo'], '23': ['tuvimos'], '24': ['tuvisteis'], '25': ['tuvieron'], '26': ['tendré'], '27': ['tendrás'], '28': ['tendrá'], '29': ['tendremos'], '30': ['tendréis'], '31': ['tendrán'], '32': ['tendría'], '33': ['tendrías'], '34': ['tendría'], '35': ['tendríamos'], '36': ['tendríais'], '37': ['tendrían'], '38': ['tenga'], '39': ['tengas'], '41': ['tenga'], '42': ['tengamos'], '43': ['tengáis'], '44': ['tengan'], '45': ['tuviera'], '46': ['tuvieras'], '47': ['tuviera'], '48': ['tuviéramos'], '49': ['tuvierais'], '50': ['tuvieran'], '51': ['tuviese'], '52': ['tuvieses'], '53': ['tuviese'], '54': ['tuviésemos'], '55': ['tuvieseis'], '56': ['tuviesen'], '57': ['tuviere'], '58': ['tuvieres'], '59': ['tuviere'], '60': ['tuviéremos'], '61': ['tuviereis'], '62': ['tuvieren'], '63': ['ten'], '64': ['tené'], '65': ['tenga'], '66': ['tengamos'], '67': ['tened'], '68': ['tengan'], '69': ['tengas'], '70': ['tenga'], '71': ['tengamos'], '72': ['tengáis'], '73': ['tengan']}
+    assert forms['1'] == {'tener'}
+    assert forms['2'] == {'teniendo'}
+    assert forms['inf_acc_1'] == {'tenerme'}
+    assert sorted(forms['inf_acc-dat_1']) == sorted(['tenérmelos', 'tenérmela', 'tenérmelas', 'tenérmelo'])
 
 def test_get_verb_forms2():
 
@@ -164,7 +166,7 @@ From {{der|es|la|attentō}}.
     word = next(wikt.ifilter_words())
     template = next(word.ifilter_templates(matches=lambda x: "es-verb" in x.name.strip()))
     forms = Data.get_forms(word)
-    assert len(forms) == 73
+    assert len(forms) == 188
 
 
 def test_get_verb_forms_noconjugation():
@@ -183,4 +185,41 @@ def test_get_verb_forms_noconjugation():
     template = next(word.ifilter_templates(matches=lambda x: "es-verb" in x.name.strip()))
     forms = Data.get_forms(word)
     assert forms == {}
+
+
+def test_inflect():
+    assert len(Data.inflect(["habl"], "-ar", "", False)) == 73
+    assert Data.inflect(["habl"], "-ar", "", False)[1] == ["hablar"]
+    assert Data.inflect(["habl"], "-ar", "", True)[1] == ["hablarse"]
+
+def test_inflect_combined():
+    forms = Data.inflect(["habl"], "-ar", "", False)
+    combined = Data.inflect_combined(forms, "-ar", "", False)
+    assert  combined == {'inf_acc_1': ['hablarme'], 'inf_acc_2': ['hablarte'], 'inf_acc_3': ['hablarlo', 'hablarla', 'hablarse'], 'inf_acc_4': ['hablarnos'], 'inf_acc_5': ['hablaros'], 'inf_acc_6': ['hablarlos', 'hablarlas', 'hablarse'], 'inf_acc_7': ['hablarse'], 'inf_dat_1': ['hablarme'], 'inf_dat_2': ['hablarte'], 'inf_dat_3': ['hablarle', 'hablarse'], 'inf_dat_4': ['hablarnos'], 'inf_dat_5': ['hablaros'], 'inf_dat_6': ['hablarles', 'hablarse'], 'ger_acc_1': ['hablándome'], 'ger_acc_2': ['hablándote'], 'ger_acc_3': ['hablándolo', 'hablándola', 'hablándose'], 'ger_acc_4': ['hablándonos'], 'ger_acc_5': ['hablándoos'], 'ger_acc_6': ['hablándolos', 'hablándolas', 'hablándose'], 'ger_acc_7': ['hablándose'], 'ger_dat_1': ['hablándome'], 'ger_dat_2': ['hablándote'], 'ger_dat_3': ['hablándole', 'hablándose'], 'ger_dat_4': ['hablándonos'], 'ger_dat_5': ['hablándoos'], 'ger_dat_6': ['hablándoles', 'hablándose'], 'imp_i2s_acc_1': ['háblame'], 'imp_i2s_acc_2': ['háblate'], 'imp_i2s_acc_3': ['háblalo', 'háblala'], 'imp_i2s_acc_4': ['háblanos'], 'imp_i2s_acc_6': ['háblalos', 'háblalas'], 'imp_i2s_dat_1': ['háblame'], 'imp_i2s_dat_2': ['háblate'], 'imp_i2s_dat_3': ['háblale'], 'imp_i2s_dat_4': ['háblanos'], 'imp_i2s_dat_6': ['háblales'], 'imp_f2s_acc_1': ['hábleme'], 'imp_f2s_acc_3': ['háblelo', 'háblela', 'háblese'], 'imp_f2s_acc_4': ['háblenos'], 'imp_f2s_acc_6': ['háblelos', 'háblelas'], 'imp_f2s_acc_7': ['háblese'], 'imp_f2s_dat_1': ['hábleme'], 'imp_f2s_dat_3': ['háblele', 'háblese'], 'imp_f2s_dat_4': ['háblenos'], 'imp_f2s_dat_6': ['hábleles'], 'imp_1p_acc_2': ['hablémoste'], 'imp_1p_acc_3': ['hablémoslo', 'hablémosla'], 'imp_1p_acc_4': ['hablémonos'], 'imp_1p_acc_5': ['hablémoos'], 'imp_1p_acc_6': ['hablémoslos', 'hablémoslas'], 'imp_1p_dat_2': ['hablémoste'], 'imp_1p_dat_3': ['hablémosle'], 'imp_1p_dat_4': ['hablémonos'], 'imp_1p_dat_5': ['hablémoos'], 'imp_1p_dat_6': ['hablémosles'], 'imp_i2p_acc_1': ['habladme'], 'imp_i2p_acc_3': ['habladlo', 'habladla'], 'imp_i2p_acc_4': ['habladnos'], 'imp_i2p_acc_5': ['hablaos'], 'imp_i2p_acc_6': ['habladlos', 'habladlas'], 'imp_i2p_acc_7': ['hablados'], 'imp_i2p_dat_1': ['habladme'], 'imp_i2p_dat_3': ['habladle'], 'imp_i2p_dat_4': ['habladnos'], 'imp_i2p_dat_5': ['hablaos'], 'imp_i2p_dat_6': ['habladles'], 'imp_f2p_acc_1': ['háblenme'], 'imp_f2p_acc_3': ['háblenlo', 'háblenla'], 'imp_f2p_acc_4': ['háblennos'], 'imp_f2p_acc_6': ['háblenlos', 'háblenlas', 'háblense'], 'imp_f2p_acc_7': ['háblense'], 'imp_f2p_dat_1': ['háblenme'], 'imp_f2p_dat_3': ['háblenle'], 'imp_f2p_dat_4': ['háblennos'], 'imp_f2p_dat_6': ['háblenles', 'háblense']}
+
+    forms = Data.inflect([], "-ir", "venir", False)
+    combined = Data.inflect_combined(forms, "-ir", "venir", False)
+    assert combined['imp_i2s_acc_4'] == ['venos']
+    assert combined['imp_i2s_dat_4'] == ['venos']
+    assert combined['imp_i2p_acc_5'] == ['veníos']
+    assert combined['imp_i2p_dat_5'] == ['veníos']
+
+    forms = Data.inflect([], "-ir", "ir", False)
+    combined = Data.inflect_combined(forms, "-ir", "ir", False)
+    assert combined['imp_i2p_dat_5'] ==  ['idos', 'iros']
+
+
+def test_create_accented_form():
+    assert Data.create_accented_form("hablame") == "háblame"
+
+    # 'ue' or 'ua' count as 1 vowel, accent goes on e or a
+    assert Data.create_accented_form("aburguesele") == "aburguésele"
+    assert Data.create_accented_form("malualo") == "málualo"
+    # unless ua_disyllabic is set
+    assert Data.create_accented_form("malualo",True) == "malúalo"
+
+    # 'ai' or 'oi' counts as 1 vowel, accent goes on a or o
+    assert Data.create_accented_form("bailame") == "báilame"
+    assert Data.create_accented_form("boilame") == "bóilame"
+    assert Data.create_accented_form("beilame") == "beílame"
 
